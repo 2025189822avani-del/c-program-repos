d94e1cf80a5763787ae40683332206d052131b5a
2025189822avani-del <2025189822.avani@ug.sharda.ac.in>
2026-04-17 17:23:04 +0530

    Update week5.c

--- week5.c
+++ week5.c
@@ -57,3 +57,63 @@ complex Mul(complex a, complex b)
     return r;
 }
 // ques 2
+struct Student{
+    char name[50];
+    int roll_no;
+    char mobile_no[15];
+    float CGPA;
+    char DoB[15];
+    char course[40];
+    int year;
+};
+void readStudent(struct Student *s)
+{
+    printf("Enter Name: ");
+    scanf(" %[^\n]", s->name);
+    printf("Enter Roll No: ");
+    scanf("%d", &s->roll_no);
+    printf("Enter Mobile No: ");
+    scanf(" %s", s->mobile_no);
+    printf("Enter CGPA: ");
+    scanf("%f", &s->CGPA);
+    printf("Enter Date of Birth (DD/MM/YYYY): ");
+    scanf(" %s", s->DoB);
+    printf("Enter Course: ");
+    scanf(" %[^\n]", s->course);
+    printf("Enter Year: ");
+    scanf("%d", &s->year);
+}
+void displayStudent(struct Student s)
+{
+    printf("Student Details");
+    printf("Name : %s\n", s.name);
+    printf("Roll No : %d\n", s.roll_no);
+    printf("Mobile No : %s\n", s.mobile_no);
+    printf("CGPA : %.2f\n", s.CGPA);
+    printf("DoB : %s\n", s.DoB);
+    printf("Course : %s\n", s.course);
+    printf("Year : %d\n", s.year);
+}
+int main()
+{
+    struct Student students[N];
+    int i, topperIndex = 0;
+    printf("Enter details of %d students:\n\n", N);
+    for (i = 0; i < N; i++) 
+    {
+        printf("Student %d:\n", i + 1);
+        readStudent(&students[i]);
+        printf("\n");
+    }
+    for (i = 1; i < N; i++) 
+    {
+        if (students[i].CGPA > students[topperIndex].CGPA) 
+        {
+            topperIndex = i;
+        }
+    }
+    printf("==\n");
+    printf("Topper of the class:\n");
+    displayStudent(students[topperIndex]);
+    return 0;
+}
